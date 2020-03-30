c97f34d31bce2d5299c33be3334dd72bb505c702
sam <samreality4@gmail.com>
2020-03-30 16:36:17 -0700

    Updates

---
+++ precache-manifest.8f7aa4b044fde5d46415609b4cff9e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b0b9ded5646448ba137e80d00c1485",
+    "url": "/mypersonalsite/index.html"
+  },
+  {
+    "revision": "547d31916ee0b7bececd",
+    "url": "/mypersonalsite/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "547d31916ee0b7bececd",
+    "url": "/mypersonalsite/static/js/2.0930b21c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mypersonalsite/static/js/2.0930b21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf5f27b90f3f3913681",
+    "url": "/mypersonalsite/static/js/main.63ec5562.chunk.js"
+  },
+  {
+    "revision": "837f619d0b96310aff06",
+    "url": "/mypersonalsite/static/js/runtime-main.868cc6b4.js"
+  }
+]);
